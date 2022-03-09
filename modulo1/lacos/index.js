52876501483da87547e26d989ab60089ac66b4e3
Bruno Araujo <brunolarj@gmail.com>
2022-03-08 23:58:48 -0300

    exercicio de lacos

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,29 @@
+// ENUNCIADO
+//1
+
+// o codigo soma valores a "i" enquanto ele for menor que 5
+// no console sera impresso "5"
+
+
+//2
+
+//19 21 23 25 27 30 
+
+//b) Nao
+
+
+//3 * ** *** ****
+
+
+
+// ESCRITA
+
+//1
+
+let bichinhos = 0 
+
+bichinhos = Number(prompt("Quantos bichinhos você tem?"))
+
+console.log(bichinhos)
+
+git status
